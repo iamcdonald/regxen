abbe0b2862356b02e3aa44539e4f1181046e4215
Iain McDonald <iain.allan.mcdonald@protonmail.com>
2024-07-30 01:19:58 +0100

    ci: add test workflow

--- test/RegXenProxy.ts
+++ test/RegXenProxy.ts
@@ -1,15 +1,16 @@
 import RegXen from "../src/index";
 let $RegXenB: Promise<typeof RegXen>;
 if (process.env.BUILT) {
-	$RegXenB = import("../dist").then((m) => m.default) as Promise<typeof RegXen>;
+  // @ts-ignore
+  $RegXenB = import("../dist").then((m) => m.default) as Promise<typeof RegXen>;
 }
 
 export default {
-	get: async (): Promise<typeof RegXen> => {
-		if (process.env.BUILT) {
-			console.log("USING BUILT");
-			return await $RegXenB;
-		}
-		return RegXen;
-	},
+  get: async (): Promise<typeof RegXen> => {
+    if (process.env.BUILT) {
+      console.log("USING BUILT");
+      return await $RegXenB;
+    }
+    return RegXen;
+  },
 };
