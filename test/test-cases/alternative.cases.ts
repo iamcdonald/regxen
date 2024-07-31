e735fe553c39a8b56475a8cefe7545b2b6af2959
Iain McDonald <iain.allan.mcdonald@protonmail.com>
2024-07-31 14:28:51 +0100

    test: update integration test assertions to be more narrow

--- test/test-cases/alternative.cases.ts
+++ test/test-cases/alternative.cases.ts
@@ -1,10 +1,14 @@
-export default [
-	{
-		regex: /a|b/,
-		description: "letter",
-	},
-	{
-		regex: /\u{1F44B}HI|BYE\u{1F44D}/u,
-		description: "unicode",
-	},
+import type { TestCase } from "./types";
+
+const cases: TestCase[] = [
+  {
+    regex: /a|b/,
+    description: "letter",
+  },
+  {
+    regex: /\u{1F44B}HI|BYE\u{1F44D}/u,
+    description: "unicode",
+  },
 ];
+
+export default cases;
