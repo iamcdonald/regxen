8a661feb83610f38002cf6bf9238fecedf5b8677
Iain McDonald <iain.allan.mcdonald@protonmail.com>
2024-07-31 14:28:51 +0100

    test: narrow integration test assertions

--- test/test-cases/group.cases.ts
+++ test/test-cases/group.cases.ts
@@ -1,42 +1,46 @@
-export default [
-	{
-		regex: /(HI[A-Z]*)?/,
-		description: "simple",
-	},
-	{
-		regex: /(HI[A-Z]*)(x)\1/,
-		description: "reference - number",
-	},
-	{
-		regex: /(?<name>HI[A-Z]*)\k<name>/,
-		description: "reference - named",
-	},
-	{
-		// positive lookahead group is ignored
-		// returns: NOPE
-		regex: /(?=NO_SHOW)NOPE/,
-		description: "assertion - positive lookahead (ignored)",
-		checkOutput: /NOPE/,
-	},
-	{
-		// negative lookahead group is ignored
-		// returns: NO_SHOW
-		regex: /(?!NO_SHOW)NO_SHOW/,
-		description: "assertion - negative lookahead (ignored)",
-		checkOutput: /NO_SHOW/,
-	},
-	{
-		// positive lookbehind group is ignored
-		// returns: NOPE
-		regex: /NOPE(?<=NO_SHOW)/,
-		description: "assertion - positive lookbehind (ignored)",
-		checkOutput: /NOPE/,
-	},
-	{
-		// negative lookbehind group is ignored
-		// returns: NO_SHOW
-		regex: /NO_SHOW(?<!NO_SHOW)/,
-		description: "assertion - negative lookbehind (ignored)",
-		checkOutput: /NO_SHOW/,
-	},
+import type { TestCase } from "./types";
+
+const cases: TestCase[] = [
+  {
+    regex: /(HI[A-Z]*)?/,
+    description: "simple",
+  },
+  {
+    regex: /(HI[A-Z]*)(x)\1/,
+    description: "reference - number",
+  },
+  {
+    regex: /(?<name>HI[A-Z]*)\k<name>/,
+    description: "reference - named",
+  },
+  {
+    // positive lookahead group is ignored
+    // returns: NOPE
+    regex: /(?=NO_SHOW)NOPE/,
+    description: "assertion - positive lookahead (ignored)",
+    checkOutput: /NOPE/,
+  },
+  {
+    // negative lookahead group is ignored
+    // returns: NO_SHOW
+    regex: /(?!NO_SHOW)NO_SHOW/,
+    description: "assertion - negative lookahead (ignored)",
+    checkOutput: /NO_SHOW/,
+  },
+  {
+    // positive lookbehind group is ignored
+    // returns: NOPE
+    regex: /NOPE(?<=NO_SHOW)/,
+    description: "assertion - positive lookbehind (ignored)",
+    checkOutput: /NOPE/,
+  },
+  {
+    // negative lookbehind group is ignored
+    // returns: NO_SHOW
+    regex: /NO_SHOW(?<!NO_SHOW)/,
+    description: "assertion - negative lookbehind (ignored)",
+    checkOutput: /NO_SHOW/,
+  },
 ];
+
+export default cases;
--- test/test-cases/meta.cases.ts
+++ test/test-cases/meta.cases.ts
@@ -1,46 +1,92 @@
-export default [
-	{
-		regex: /\d/,
-		description: "digit",
-	},
-	{
-		regex: /\w/,
-		description: "word",
-	},
-	{
-		regex: /\s/,
-		description: "white-space",
-	},
-	{
-		regex: /./,
-		description: "any",
-	},
-	{
-		regex: /\p{ASCII}/u,
-		description: "property - lone",
-	},
-	{
-		regex: /\p{Script=Hiragana}/u,
-		description: "property - with key & value",
-	},
-	{
-		regex: /\D/,
-		description: "digit - negate",
-	},
-	{
-		regex: /\W/,
-		description: "word - negate",
-	},
-	{
-		regex: /\S/,
-		description: "white-space - negate",
-	},
-	{
-		regex: /\P{ASCII}/u,
-		description: "property (lone) - negate",
-	},
-	{
-		regex: /\P{Script=Hiragana}/u,
-		description: "property (with key & value) - negate",
-	},
+import type { TestCase } from "./types";
+
+const cases: TestCase[] = [
+  {
+    regex: /\d/,
+    description: "digit",
+  },
+  {
+    regex: /\w/,
+    description: "word",
+  },
+  {
+    regex: /\s/,
+    description: "white-space",
+  },
+  {
+    regex: /./,
+    description: "any",
+  },
+  {
+    regex: /\p{ASCII}/u,
+    description: "property - lone",
+    options: {
+      unicode: {
+        filter: /[A\u{1F3FB}]/v,
+      },
+    },
+    checkOutput: /A/,
+  },
+  {
+    regex: /\p{Script=Hiragana}/v,
+    description: "property - with key & value",
+    options: {
+      unicode: {
+        filter: /[A-Zを]/v,
+      },
+    },
+    checkOutput: /を/,
+  },
+  {
+    regex: /\D/,
+    description: "digit - negate",
+    options: {
+      unicode: {
+        filter: /[\dA]/,
+      },
+    },
+    checkOutput: /A/,
+  },
+  {
+    regex: /\W/,
+    description: "word - negate",
+    options: {
+      unicode: {
+        filter: /[\w{]/,
+      },
+    },
+    checkOutput: /{/,
+  },
+  {
+    regex: /\S/,
+    description: "white-space - negate",
+    options: {
+      unicode: {
+        filter: /[\s\{]/v,
+      },
+    },
+    checkOutput: /{/,
+  },
+  {
+    regex: /\P{ASCII}/v,
+    description: "property (lone) - negate",
+    options: {
+      unicode: {
+        filter: /[\p{ASCII}\u{1F3FB}]/v,
+      },
+    },
+    checkOutput: /\u{1F3FB}/v,
+  },
+  {
+    regex: /\P{Script=Hiragana}/v,
+    description: "property (with key & value) - negate",
+    options: {
+      unicode: {
+        filter: /[\p{Script=Hiragana}A]/v,
+      },
+    },
+    checkOutput: /A/,
+  },
 ];
+
+export default cases;
