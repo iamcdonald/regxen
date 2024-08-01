e17d12b2e99c9e3b7b4ac226b24dec31f1ed4ef9
Iain McDonald <iain.allan.mcdonald@protonmail.com>
2024-08-01 10:00:25 +0100

    test: add unsupported pattern test cases

--- test/test-cases/group.cases.ts
+++ test/test-cases/group.cases.ts
@@ -11,32 +11,4 @@ export default [
     regex: { source: "(?<name>HI[A-Z]*)\\k<name>" },
     description: "reference - named",
   },
-  {
-    // positive lookahead group is ignored
-    // returns: NOPE
-    regex: { source: "(?=NO_SHOW)NOPE" },
-    description: "assertion - positive lookahead (ignored)",
-    checkOutput: { source: "NOPE" },
-  },
-  {
-    // negative lookahead group is ignored
-    // returns: NO_SHOW
-    regex: { source: "(?!NO_SHOW)NO_SHOW" },
-    description: "assertion - negative lookahead (ignored)",
-    checkOutput: { source: "NO_SHOW" },
-  },
-  {
-    // positive lookbehind group is ignored
-    // returns: NOPE
-    regex: { source: "NOPE(?<=NO_SHOW)" },
-    description: "assertion - positive lookbehind (ignored)",
-    checkOutput: { source: "NOPE" },
-  },
-  {
-    // negative lookbehind group is ignored
-    // returns: NO_SHOW
-    regex: { source: "NO_SHOW(?<!NO_SHOW)" },
-    description: "assertion - negative lookbehind (ignored)",
-    checkOutput: { source: "NO_SHOW" },
-  },
 ];
